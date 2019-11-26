335473a89a4f32d3b6cce42ce1152743dcf411cf
dbrvskyi <dubrovskiievgeniy@gmail.com>
2019-11-26 19:36:39 +0200

    refactor: Remove useless home-routing

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
 
 @NgModule({
+  imports: [CommonModule],
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  exports: [HomeComponent],
 })
 export class HomeModule {}
